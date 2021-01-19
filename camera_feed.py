812e5963db907b353ff76d18003b3220851b1e1c
Sebastian Ruiz <sebastian.ruiz@uni-goettingen.de>
2021-01-19 14:15:24 +0100

    camera feed

---
+++ camera_feed.py
@@ -0,0 +1,28 @@
+from pypylon import pylon
+from calibration import *
+
+def camera_feed(undistort=True):
+    # conecting to the first available camera
+    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+    # Grabing Continusely (video) with minimal delay
+    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+    converter = pylon.ImageFormatConverter()
+
+    # converting to opencv bgr format
+    converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+    converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+    while camera.IsGrabbing():
+
+        grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+        if grabResult.GrabSucceeded():
+            # Access the image data
+            image = converter.Convert(grabResult)
+            img = image.GetArray()
+
+            if undistort:
+                img = calibration.undistort_imgs(img)  # resizes and undistorts image
+
+        grabResult.Release()
