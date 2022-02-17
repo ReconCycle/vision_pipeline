Fix angle calculation

pipeline_v2.py
@@ -18,6 +18,20 @@ from yolact.data import cfg
 
 import json
 
+def quaternion_multiply(quaternion1, quaternion0):
+        x0, y0, z0, w0 = quaternion0
+        x1, y1, z1, w1 = quaternion1
+
+        # This quat is W X Y Z 
+        out_quat = np.array([-x1*x0 - y1*y0 - z1*z0 + w1*w0,
+                     x1*w0 + y1*z0 - z1*y0 + w1*x0,
+                    -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+                     x1*y0 - y1*x0 + z1*w0 + w1*z0], dtype=np.float64)
+
+        # This quat is X Y Z W
+        out = out_quat[[1,2,3,0]]
+        return out
+
 class Pipeline:
     def __init__(self):
         # 1. load camera calibration files
@@ -28,7 +42,7 @@ class Pipeline:
 
         # 3. object detection
         self.object_detection = ObjectDetection()
-
+    
 
     def process_img(self, img, fps=None):
         with torch.no_grad():
@@ -106,14 +120,30 @@ class Pipeline:
                 angle = (np.arctan2(unit_vector_1[1], unit_vector_1[0]) -
                          np.arctan2(unit_vector_2[1], unit_vector_2[0]))
 
+
                 # If angle is too negative, add 180 degrees
                 if (angle * 180 / np.pi) < -30:
                     angle = angle + np.pi
                 # Logger.loginfo("Angle: {}".format(angle * 180 / np.pi))
 
+                # Below code works but z-axis is incorrect, should be rotated by 180 degs
+                angle = -angle
+
                 rot_quat = np.concatenate((np.sin(angle/2)*np.array([0,0,1]), 
                                            np.array([np.cos(angle/2)])))
+
+                #rot_quat = np.concatenate((np.sin(angle/2)*np.array([0,0,-1]), 
+                #                           np.array([-np.cos(angle/2)])))
+
+                #Rotate around x-axis by 180 degs
+                rot_quat = quaternion_multiply(np.array([0,1,0,0]), rot_quat)
+                
+               
+                #Rotate around z-axis by 180 degs
+                #rot_quat = quaternion_multiply(np.array([0,0,1,0]), rot_quat)
+
                 detection["obb_rot_quat"] = rot_quat.tolist()
+                # detection["obb_rot_quat"] = np.array([[1,0,0,0]]).tolist()
 
         return labelled_img, detections
 