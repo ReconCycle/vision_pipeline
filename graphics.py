quaternions correct

graphics.py
@@ -141,15 +141,10 @@ def get_labelled_img(img, masks, detections, h=None, w=None, undo_transform=Fals
     for detection in detections:
         if detection.center_px is not None:
             cv2.circle(img_numpy, tuple(detection.center_px), 5, (0, 255, 0), -1)
-            cv2.drawContours(img_numpy, [detection.obb_px], 0, (0, 255, 0), 2)
-            
-            # get the rotation as an angle
-            quat = detection.tf_px.rotation
-            quat_as_arr = lambda o: np.array([o.x, o.y, o.z, o.w])
-            rot = Rotation.from_quat(quat_as_arr(quat)).as_euler('zyx', degrees=True)
+            cv2.drawContours(img_numpy, [detection.obb_px], 0, (0, 255, 0), 2)          
             
             # draw the arrow
-            point2 = rotated_line(tuple(detection.center_px), rot[2], 60)
+            point2 = rotated_line(tuple(detection.center_px), detection.angle_px, 60)
             cv2.arrowedLine(img_numpy, tuple(detection.center_px), point2, (0, 255, 0), 3, tipLength = 0.5)
             
             # cv2.drawContours(img_numpy, [detection.mask_contour], 0, (0, 255, 0), 2)

obb.py
@@ -192,8 +192,6 @@ def get_obb_using_cv(contour, img=None):
     else:
         correct_rot = changing_rot - 90
 
-    correct_quat = Rotation.from_euler('x', correct_rot, degrees=True).as_quat()
-
     correct_height = changing_height
     correct_width = changing_width
     if changing_height < changing_width:
@@ -209,7 +207,7 @@ def get_obb_using_cv(contour, img=None):
         box = clip_box_to_img_shape(box, img.shape)
         # box = np.clip(box, a_min=np.asarray([0, 0]), a_max=np.asarray(img.shape[:2])[::-1] - 1) 
     
-    return box, center, correct_quat
+    return box, center, correct_rot
 
 def box_w_h(box):
     return np.linalg.norm(box[0] - box[1]), np.linalg.norm(box[1] - box[2])
@@ -232,10 +230,10 @@ def get_obb_from_contour(contour, img=None):
     """
     
     # https://stackoverflow.com/questions/13542855/algorithm-to-find-the-minimum-area-rectangle-for-given-points-in-order-to-comput/33619018#33619018
-    corners, center, rot_quat = get_obb_using_cv(contour, img)
+    corners, center, rot = get_obb_using_cv(contour, img)
     # corners, center, rot_quat =  get_obb_using_eig(contour)
 
     # better_rot_quat = rot_quat
     # if corners is not None:
     #     better_rot_quat = better_quaternion(corners)
-    return corners, center, rot_quat
+    return corners, center, rot

object_detection.py
@@ -51,11 +51,6 @@ class ObjectDetection:
         self.tracker = BYTETracker(self.tracker_args)
         self.fps_graphics = -1.
         self.fps_objdet = -1.
-        
-        # convert class names to enums
-        # labels = enum.IntEnum('label', self.dataset.class_names, start=0)
-        # self.labels = labels
-        
 
     def get_prediction(self, img_path, depth_img=None, worksurface_detection=None, extra_text=None, camera_info=None):
         t_start = time.time()
@@ -116,19 +111,30 @@ class ObjectDetection:
         
         markers = MarkerArray()
         markers.markers = []
+        markers.markers.append(self.delete_all_markers())
         
         poses = PoseArray()
         poses.header.frame_id = self.frame_id
         poses.header.stamp = rospy.Time.now()
         
         # calculate the oriented bounding boxes
-        for detection in detections:           
-            corners_px, center_px, rot_quat = obb.get_obb_from_contour(detection.mask_contour, img_path)
+        for detection in detections:                
+            corners_px, center_px, angle = obb.get_obb_from_contour(detection.mask_contour, img_path)
             detection.obb_px = corners_px
             detection.center_px = center_px
+            detection.angle_px = angle
+            
+            if worksurface_detection is not None:
+                # basler, w.r.t. vision_module
+                rotation_axis='z'
+                rot_quat = Rotation.from_euler(rotation_axis, angle, degrees=True).inv().as_quat()
+            else:
+                # realsense module, w.r.t. realsense_link
+                rotation_axis='x'
+                rot_quat = Rotation.from_euler(rotation_axis, angle, degrees=True).as_quat()
             
             # todo: obb_3d
-            detection.tf_px = Transform(Vector3(*center_px, 0), Quaternion(*rot_quat))     
+            detection.tf_px = Transform(Vector3(*center_px, 0), Quaternion(*rot_quat))
             
             if worksurface_detection is not None and corners_px is not None:
                 center = worksurface_detection.pixels_to_meters(center_px)
@@ -182,48 +188,38 @@ class ObjectDetection:
                     height = changing_width
                     width = changing_height
                 
-                marker = self.make_marker(detection.tf, self.object_depth, height, width, detection.id, detection.label)
+                if worksurface_detection is not None:
+                    # basler, w.r.t. worksurface_module
+                    marker = self.make_marker(detection.tf, height, width, self.object_depth, detection.id, detection.label)
+                else:
+                    # realsense, w.r.t. realsense_link
+                    marker = self.make_marker(detection.tf, self.object_depth, height, width, detection.id, detection.label)
                 markers.markers.append(marker)
             
             # draw the poses and tfs
             if detection.tf is not None:
                 # change the angle of the pose so that it looks good for visualisation
-                rot_multiplier = Rotation.from_euler('xyz', [90, 0, 90], degrees=True).as_quat()
+                if worksurface_detection is not None:
+                    # basler, w.r.t. worksurface_module
+                    rot_multiplier = Rotation.from_euler('xyz', [0, 0, 0], degrees=True).as_quat()
+                else:
+                    # realsense, w.r.t. realsense_link
+                    rot_multiplier = Rotation.from_euler('xyz', [90, 0, 90], degrees=True).as_quat()
                 pretty_rot = obb.quaternion_multiply(rot_multiplier, rot_quat)
                 
                 pose = Pose()
                 pose.position = detection.tf.translation
                 pose.orientation = Quaternion(*pretty_rot)
-                # pose.orientation = detection.tf.rotation
                 poses.poses.append(pose)
                 
-                # publish transforms of objects
-                rot_to_arr = lambda o: np.array([o.x, o.y, o.z, o.w])
-                tra_to_arr = lambda o: np.array([o.x, o.y, o.z])  
-                
-                # ! use stamped transform
-                # br = tf.TransformBroadcaster()
-                # br.sendTransform(tra_to_arr(detection.tf.translation),
-                #     rot_to_arr(detection.tf.rotation),
-                #     rospy.Time.now(),
-                #     "obj_"+ str(detection.id),
-                #     "realsense_link")
-                
+                #! move this to the pipeline.py files
                 br = tf2_ros.TransformBroadcaster()
                 t = TransformStamped()
 
                 t.header.stamp = rospy.Time.now()
                 t.header.frame_id = self.frame_id
                 t.child_frame_id = "obj_"+ str(detection.id)
-                # t.transform.translation.x = msg.x
-                # t.transform.translation.y = msg.y
-                # t.transform.translation.z = 0.0
                 t.transform = detection.tf
-                # q = tf_conversions.transformations.quaternion_from_euler(0, 0, msg.theta)
-                # t.transform.rotation.x = q[0]
-                # t.transform.rotation.y = q[1]
-                # t.transform.rotation.z = q[2]
-                # t.transform.rotation.w = q[3]
 
                 br.sendTransform(t)
                 
@@ -250,7 +246,7 @@ class ObjectDetection:
         marker.action = Marker.ADD
         marker.header.frame_id = self.frame_id
         marker.header.stamp = rospy.Time.now()
-        marker.ns = 'marker_test_%d' % Marker.CUBE
+        marker.ns = 'detection_%d' % Marker.CUBE
         marker.id = id
         marker.type = Marker.CUBE
         
@@ -269,3 +265,14 @@ class ObjectDetection:
         marker.color.a = 0.75
         
         return marker
+    
+    def delete_all_markers(self):
+        marker = Marker()
+        marker.action = Marker.DELETEALL
+        marker.header.frame_id = self.frame_id
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = 'detection_%d' % Marker.CUBE
+        marker.type = Marker.CUBE
+        
+        return marker
+        

pipeline_basler.py
@@ -15,14 +15,16 @@ from cv_bridge import CvBridge
 from std_msgs.msg import String
 from camera_control_msgs.srv import SetSleeping
 from visualization_msgs.msg import MarkerArray
-from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import PoseArray, TransformStamped
+import tf2_ros
+import tf
 
 from context_action_framework.msg import Detection as ROSDetection
 from context_action_framework.msg import Detections as ROSDetections
 from context_action_framework.types import detections_to_ros
 
 class BaslerPipeline:
-    def __init__(self, yolact, dataset, camera_topic="basler", node_name="vision_basler", wait_for_services=True):     
+    def __init__(self, yolact, dataset, camera_topic="basler", node_name="vision_basler", image_topic="image_rect_color", wait_for_services=True):     
         self.rate = rospy.Rate(1) # fps
 
         # don't automatically start
@@ -30,6 +32,7 @@ class BaslerPipeline:
 
         self.camera_topic = camera_topic
         self.node_name = node_name
+        self.image_topic = image_topic # will subscribe to camera_topic / image_topic
 
         self.img_sub = None
 
@@ -43,6 +46,10 @@ class BaslerPipeline:
         self.detections = None
         self.markers = None
         self.poses = None
+        
+        self.frame_id = "vision_module"
+        
+        self.create_static_tf(self.frame_id)
 
         print("creating camera subscribers...")
         self.create_camera_subscribers()
@@ -56,7 +63,7 @@ class BaslerPipeline:
         print("waiting for pipeline to be enabled...")
 
     def init_basler_pipeline(self, yolact, dataset):
-        self.object_detection = ObjectDetection(yolact, dataset)
+        self.object_detection = ObjectDetection(yolact, dataset, self.frame_id)
         
         self.worksurface_detection = None
 
@@ -66,14 +73,14 @@ class BaslerPipeline:
         self.img_id += 1
 
     def create_camera_subscribers(self):
-        img_topic = "/" + self.camera_topic + "/image_rect_color"
+        img_topic = "/" + self.camera_topic + "/" + self.image_topic
         self.img_sub = rospy.Subscriber(img_topic, Image, self.img_from_camera_callback)
 
     def create_service_client(self):
         try:
             rospy.wait_for_service("/" + self.camera_topic + "/set_sleeping", 2) # 2 seconds
         except rospy.ROSException as e:
-            print("[red]Couldn't find to service![/red]")
+            print("[red]Couldn't find to service! /" + self.camera_topic + "/set_sleeping[/red]")
     
         self.camera_service = rospy.ServiceProxy("/" + self.camera_topic + "/set_sleeping", SetSleeping)
 
@@ -91,7 +98,7 @@ class BaslerPipeline:
         self.labelled_img_pub.publish(self.br.cv2_to_imgmsg(img))
         self.detections_pub.publish(ros_detections)
         self.markers_pub.publish(markers)
-        self.markers_pub.publish(poses)
+        self.poses_pub.publish(poses)
 
     def enable_camera(self, state):
         # enable = True, but the topic is called set_sleeping, so the inverse
@@ -103,7 +110,7 @@ class BaslerPipeline:
             else:
                 print("disabled camera:", res.success)
         except rospy.ServiceException as e:
-            print("Service call failed (state " + str(state) + "): ", e)
+            print("[red]Service call failed (state " + str(state) + "):[/red]", e)
 
     def enable(self, state):
         self.enable_camera(state)
@@ -167,3 +174,24 @@ class BaslerPipeline:
         # json_detections = json.dumps(detections, cls=EnhancedJSONEncoder)
         
         return labelled_img, detections, markers, poses
+
+    def create_static_tf(self, frame_id):
+        pass
+        broadcaster = tf2_ros.StaticTransformBroadcaster()
+        static_transformStamped = TransformStamped()
+
+        static_transformStamped.header.stamp = rospy.Time.now()
+        static_transformStamped.header.frame_id = "world"
+        static_transformStamped.child_frame_id = frame_id
+
+        static_transformStamped.transform.translation.x = float(0)
+        static_transformStamped.transform.translation.y = float(0)
+        static_transformStamped.transform.translation.z = float(0)
+
+        quat = tf.transformations.quaternion_from_euler(float(0),float(0),float(0))
+        static_transformStamped.transform.rotation.x = quat[0]
+        static_transformStamped.transform.rotation.y = quat[1]
+        static_transformStamped.transform.rotation.z = quat[2]
+        static_transformStamped.transform.rotation.w = quat[3]
+
+        broadcaster.sendTransform(static_transformStamped)

work_surface_detection_opencv.py
@@ -63,7 +63,6 @@ class WorkSurfaceDetection:
         self.points_px_dict = None
         self.points_m_dict = None
         
-
         if isinstance(img, str):
             img = np.array(cv2.imread(img))
         else:
@@ -96,14 +95,14 @@ class WorkSurfaceDetection:
         }
                 
         self.points_m_dict = {
-            'corner0': [0, 0],                       # top-left
-            'corner1': [0.6, 0],                     # top-right
-            'corner2': [0.6, 0.6],                   # bottom-right
-            'corner3': [0, 0.6],                     # bottom-left
-            'bolt0': [0.035, 0.035],                 # top-left
-            'bolt1': [0.6 - 0.035, 0.035],           # top-right
-            'bolt2': [0.6 - 0.035, 0.6 - 0.035],     # bottom-right
-            'bolt3': [0.035, 0.6 - 0.035],           # bottom-left            
+            'corner0': [0, 0.6],                       # top-left
+            'corner1': [0.6, 0.6],                     # top-right
+            'corner2': [0.6, 0],                   # bottom-right
+            'corner3': [0, 0],                     # bottom-left
+            'bolt0': [0.035, 0.6 - 0.035],                 # top-left
+            'bolt1': [0.6 - 0.035, 0.6 - 0.035],           # top-right
+            'bolt2': [0.6 - 0.035, 0.035],     # bottom-right
+            'bolt3': [0.035, 0.035],           # bottom-left          
             'calibrationmount0': [0.3, 0.03],        # top-center
             'calibrationmount1': [0.6 - 0.03, 0.3],  # left-center
             'calibrationmount2': [0.3, 0.6 - 0.03],  # bottom-center
@@ -347,7 +346,7 @@ class WorkSurfaceDetection:
 
     def pixels_to_meters(self, coords):
         if isinstance(coords, tuple) or len(coords.shape) == 1:
-            # single coordinate pair.
+            # single coordinate pair.           
             return self.coord_transform(np.array([coords]))[0]
         else:
             # assume array of coordinate pairs. 