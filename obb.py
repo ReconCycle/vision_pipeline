fix hca_back rotation to be in direction of battery

obb.py
@@ -66,157 +66,6 @@ def quaternion_multiply(quaternion1, quaternion0):
                      -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
                      x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
 
-# def quaternion_multiply(quaternion1, quaternion0):
-#         x0, y0, z0, w0 = quaternion0
-#         x1, y1, z1, w1 = quaternion1
-
-#         # This quat is W X Y Z
-#         out_quat = np.array([-x1*x0 - y1*y0 - z1*z0 + w1*w0,
-#                      x1*w0 + y1*z0 - z1*y0 + w1*x0,
-#                     -x1*z0 + y1*w0 + z1*x0 + w1*y0,
-#                      x1*y0 - y1*x0 + z1*w0 + w1*z0], dtype=np.float64)
-
-#         # This quat is X Y Z W
-#         out = out_quat[[1,2,3,0]]
-#         return out
-
-# def better_quaternion(obb_corners):
-#     # for detection in detections:
-#     corners = np.array(obb_corners)
-#     distances = []
-#     # Logger.loginfo("{}".format(corners))
-    
-#     # sanity check
-#     if corners.shape != (4, 2):
-#         return None
-
-#     first_corner = corners[0]
-
-#     for ic, corner in enumerate(corners):
-#         distances.append(np.linalg.norm(corner - first_corner))
-#     # Logger.loginfo("Distances: {}".format(distances))
-#     distances = np.array(distances)
-#     idx_edge = distances.argsort()[-2]
-#     # Logger.loginfo("Index of edge: {}".format(idx_edge))
-#     second_corner = corners[idx_edge]
-
-#     highest_y = np.argmax([first_corner[1], second_corner[1]])
-#     if highest_y == 0:
-#         vector_1 = first_corner - second_corner
-#     elif highest_y == 1:
-#         vector_1 = second_corner - first_corner
-
-#     unit_vector_1 = vector_1 / np.linalg.norm(vector_1)
-#     unit_vector_2 = np.array([0, 1])
-#     # Logger.loginfo("Vectors: {}, {}".format(vector_1, unit_vector_2))
-
-#     angle = (np.arctan2(unit_vector_1[1], unit_vector_1[0]) -
-#                 np.arctan2(unit_vector_2[1], unit_vector_2[0]))
-
-
-#     # If angle is too negative, add 180 degrees
-#     if (angle * 180 / np.pi) < -30:
-#         angle = angle + np.pi
-#     # Logger.loginfo("Angle: {}".format(angle * 180 / np.pi))
-
-#     # Below code works but z-axis is incorrect, should be rotated by 180 degs
-#     angle = -angle
-
-#     obb_rot_quat = np.concatenate((np.sin(angle/2)*np.array([0,0,1]),
-#                                 np.array([np.cos(angle/2)])))
-
-#     #rot_quat = np.concatenate((np.sin(angle/2)*np.array([0,0,-1]),
-#     #                           np.array([-np.cos(angle/2)])))
-
-#     #Rotate around x-axis by 180 degs
-#     obb_rot_quat = quaternion_multiply(np.array([0,1,0,0]), obb_rot_quat)
-
-#     #Rotate around z-axis by 180 degs
-#     #obb_rot_quat = quaternion_multiply(np.array([0,0,1,0]), obb_rot_quat)
-
-#     # new_obb_rot_quat = obb_rot_quat.tolist()
-#     # detection.obb_rot_quat = np.array([[1,0,0,0]]).tolist()
-    
-#     return obb_rot_quat
-    
-
-def get_obb_using_eig(points, calcconvexhull=True):
-    """ given a set of points, calculate the oriented bounding 
-    box. 
-    
-    Parameters:
-    points: numpy array of point coordinates with shape (n,2)
-            where n is the number of points
-    calcconvexhull: boolean, calculate the convex hull of the 
-            points before calculating the bounding box. You typically
-            want to do that unless you know you are passing in a convex
-            point set
-    Output:
-        tuple of corners, centre
-    """
-
-    if points.size == 0 or len(points) < 4:
-        return None, None, None
-
-    # start_convexhull = time.time()
-
-    if calcconvexhull:
-        try:
-            _ch = ConvexHull(points)
-        except:
-            # something went wrong
-            return None, None, None
-        points = _ch.points[_ch.vertices]
-        
-    # fps_convexhull = 1.0 / (time.time() - start_convexhull)
-    # print("fps_convexhull", fps_convexhull)
-    
-    #? could we somehow use max and min points instead?
-    # start_maths = time.time()
-
-    #? maybe we can use an approximation for eig instead.
-    cov_points = np.cov(points,y = None,rowvar = 0,bias = 1)
-    v, vect = np.linalg.eig(cov_points)
-    tvect = np.transpose(vect)
-
-    #use the inverse of the eigenvectors as a rotation matrix and
-    # rotate the points so they align with the x and y axes
-    points_rotated = np.dot(points,np.linalg.inv(tvect))
-    # get the minimum and maximum x and y 
-    mina = np.min(points_rotated,axis=0)
-    maxa = np.max(points_rotated,axis=0)
-    diff = (maxa - mina)*0.5
-    # the centre is just half way between the min and max xy
-    center = mina + diff
-
-    # get the 4 corners by subtracting and adding half the bounding boxes height and width to the center
-    corners = np.array([center+[-diff[0],-diff[1]],center+[diff[0],-diff[1]],center+[diff[0],diff[1]],center+[-diff[0],diff[1]]])
-    # use the the eigenvectors as a rotation matrix and
-    # rotate the corners and the center back
-    corners = np.dot(corners,tvect)
-    center = np.dot(center,tvect)
-
-    # change to ints, and change order
-    corners = np.round(corners).astype(int)
-    center = np.round(center).astype(int)
-    # corners[:, 0], corners[:, 1] = corners[:, 1], corners[:, 0].copy()
-    # center[0], center[1] = center[1], center[0].copy()
-
-    # convert 2d rotation matrix to a 3d rotation matrix the rotation is around the z-axis
-    # the upper left corner of the 3d rotation matrix is the 2d rotation matrix
-    rot_matrix = np.identity(3)
-    rot_matrix[:2, :2] = tvect
-
-    rotation_obj = Rotation.from_matrix(rot_matrix)
-    rot_quat = rotation_obj.as_quat()
-    # degrees = rotation_obj.as_euler('xyz', degrees=True)
-    # print("degrees", degrees)
-    
-    # fps_maths = 1.0 / (time.time() - start_maths)
-    # print("fps_maths", fps_maths)
-
-    return corners, center, rot_quat
-
 
 def get_obb_using_cv(contour, img=None):
 
@@ -295,3 +144,80 @@ def get_obb_from_mask(mask):
         return get_obb_from_contour(mask_contour)
 
     return None, None, None
+
+# def get_obb_using_eig(points, calcconvexhull=True):
+#     """ given a set of points, calculate the oriented bounding 
+#     box. 
+    
+#     Parameters:
+#     points: numpy array of point coordinates with shape (n,2)
+#             where n is the number of points
+#     calcconvexhull: boolean, calculate the convex hull of the 
+#             points before calculating the bounding box. You typically
+#             want to do that unless you know you are passing in a convex
+#             point set
+#     Output:
+#         tuple of corners, centre
+#     """
+
+#     if points.size == 0 or len(points) < 4:
+#         return None, None, None
+
+#     # start_convexhull = time.time()
+
+#     if calcconvexhull:
+#         try:
+#             _ch = ConvexHull(points)
+#         except:
+#             # something went wrong
+#             return None, None, None
+#         points = _ch.points[_ch.vertices]
+        
+#     # fps_convexhull = 1.0 / (time.time() - start_convexhull)
+#     # print("fps_convexhull", fps_convexhull)
+    
+#     #? could we somehow use max and min points instead?
+#     # start_maths = time.time()
+
+#     #? maybe we can use an approximation for eig instead.
+#     cov_points = np.cov(points,y = None,rowvar = 0,bias = 1)
+#     v, vect = np.linalg.eig(cov_points)
+#     tvect = np.transpose(vect)
+
+#     #use the inverse of the eigenvectors as a rotation matrix and
+#     # rotate the points so they align with the x and y axes
+#     points_rotated = np.dot(points,np.linalg.inv(tvect))
+#     # get the minimum and maximum x and y 
+#     mina = np.min(points_rotated,axis=0)
+#     maxa = np.max(points_rotated,axis=0)
+#     diff = (maxa - mina)*0.5
+#     # the centre is just half way between the min and max xy
+#     center = mina + diff
+
+#     # get the 4 corners by subtracting and adding half the bounding boxes height and width to the center
+#     corners = np.array([center+[-diff[0],-diff[1]],center+[diff[0],-diff[1]],center+[diff[0],diff[1]],center+[-diff[0],diff[1]]])
+#     # use the the eigenvectors as a rotation matrix and
+#     # rotate the corners and the center back
+#     corners = np.dot(corners,tvect)
+#     center = np.dot(center,tvect)
+
+#     # change to ints, and change order
+#     corners = np.round(corners).astype(int)
+#     center = np.round(center).astype(int)
+#     # corners[:, 0], corners[:, 1] = corners[:, 1], corners[:, 0].copy()
+#     # center[0], center[1] = center[1], center[0].copy()
+
+#     # convert 2d rotation matrix to a 3d rotation matrix the rotation is around the z-axis
+#     # the upper left corner of the 3d rotation matrix is the 2d rotation matrix
+#     rot_matrix = np.identity(3)
+#     rot_matrix[:2, :2] = tvect
+
+#     rotation_obj = Rotation.from_matrix(rot_matrix)
+#     rot_quat = rotation_obj.as_quat()
+#     # degrees = rotation_obj.as_euler('xyz', degrees=True)
+#     # print("degrees", degrees)
+    
+#     # fps_maths = 1.0 / (time.time() - start_maths)
+#     # print("fps_maths", fps_maths)
+
+#     return corners, center, rot_quat

object_detection.py
@@ -25,12 +25,17 @@ from visualization_msgs.msg import Marker, MarkerArray
 import rospy
 import tf
 import tf2_ros
+from tf.transformations import quaternion_from_euler, quaternion_multiply
+
+import ros_numpy
 
 
 class ObjectDetection:
-    def __init__(self, config, yolact, dataset, object_reid, camera, frame_id=""):
+    def __init__(self, config, camera_config, yolact, dataset, object_reid, camera, frame_id=""):
         
         self.config = config
+        self.camera_config = camera_config
+        
         self.yolact = yolact
         self.dataset = dataset
         self.camera = camera
@@ -129,6 +134,7 @@ class ObjectDetection:
                 rot_quat = Rotation.from_euler('xyz', [0, 0, angle], degrees=True).inv().as_quat() # ? why inverse?
             else:
                 # detections from realsense, w.r.t. realsense camera
+                # rotate 180 degrees because camera is pointing down
                 rot_quat = Rotation.from_euler('xyz', [180, 0, angle], degrees=True).as_quat()
             
             # todo: obb_3d
@@ -138,8 +144,8 @@ class ObjectDetection:
                 center = worksurface_detection.pixels_to_meters(center_px)
                 corners = worksurface_detection.pixels_to_meters(corners_px)
                 
-                detection.center = center
-                detection.tf = Transform(Vector3(*center, 0), Quaternion(*rot_quat))
+                detection.center = np.array([*center, 0])
+                detection.tf = Transform(Vector3(*detection.center), Quaternion(*rot_quat))
                 detection.box = worksurface_detection.pixels_to_meters(detection.box_px)
                 detection.obb = corners
                 detection.polygon = worksurface_detection.pixels_to_meters(detection.polygon_px, depth=self.object_depth)
@@ -179,9 +185,10 @@ class ObjectDetection:
                 detection.tf = None
         
         
-        # todo: remove objects that don't fit certain constraints, eg. too small, too thin, too big
-        # todo: we could show these in a different colour for debugging
-        # todo: we should work in meters when filtering based on size!
+        # TODO: remove objects that don't fit certain constraints, eg. too small, too thin, too big
+        # TODO: we could show these in a different colour for debugging
+        # TODO: we should work in meters when filtering based on size!
+        # TODO: filter out duplicate detections
         for detection in detections:
             if detection.polygon is not None and detection.polygon.area < 0.1:
                 pass
@@ -191,15 +198,60 @@ class ObjectDetection:
         # form groups, adds group_id property to detections
         graph_relations.make_groups()
         
-        # todo: track groups
+        # print("groups:", graph_relations.list_wc_components)
+        print("groups by tracking id:", graph_relations.list_wc_components_t)
+        
+        # TODO: track groups
+        # TODO: based on groups, orientate HCA to always point the same way, dependent on battery position in the device.
+        for group in graph_relations.groups:
+            hca_back = graph_relations.get_first(group, Label.hca_back)
+            battery = graph_relations.get_first(group, Label.battery)
+            if hca_back is not None and battery is not None:
+                if graph_relations.is_inside(battery, hca_back):
+                    
+                    print("hca_back", hca_back.tracking_id, "and battery:", battery.tracking_id)
+                    
+                    hca_back_np_tf = ros_numpy.numpify(hca_back.tf)
+                    battery_np_tf = ros_numpy.numpify(battery.tf)
+                    print("hca_back_np_tf", hca_back_np_tf)
+                    print("battery_np_tf", battery_np_tf)
+                    
+                    inv_hca = np.linalg.inv(hca_back_np_tf)
+                    
+                    # prod_np_tf = np.dot(hca_back_np_tf, battery_np_tf)
+                    prod_np_tf = np.dot(inv_hca, battery_np_tf)
+                    prod_tf = ros_numpy.msgify(Transform, prod_np_tf)
+                    
+                    print("prod_tf.translation", prod_tf.translation)
+                    print("prod_tf.rotation", prod_tf.rotation)
+                    
+                    # now we check if prod_tf.translation.x > 0, in which case the battery is in the positive direction of the center of the HCA.
+                    if prod_tf.translation.x < 0:
+                        pass
+                        # fix rotation
+                        print("[red] Fix rotation")
+
+                        quat = ros_numpy.numpify(hca_back.tf.rotation)
+                        quat_180 = quaternion_from_euler(0, 0, np.pi)
+                        quat_new = quaternion_multiply(quat_180, quat)
+                        
+                        # write new tf
+                        hca_back.tf = Transform(Vector3(*hca_back.center), Quaternion(*quat_new))
+                        hca_back.tf_px = Transform(Vector3(*center_px, 0), Quaternion(*quat_new))
+                        
+                        # TODO: Also fix other internals like pcb, pcb_uncovered
+                        # TODO: update the direction on the image as well        
+            
+            
+        # if device contains battery:
+            # possibly flip orientation
         
         if self.config.reid:
             # object re-id
             self.object_reid.process_detection(colour_img, detections, graph_relations, visualise=True)
         
         graph_img = None
-        if (self.camera == Camera.basler and self.config.basler.publish_graph_img) \
-            or (self.camera == Camera.realsense and self.config.realsense.publish_graph_img):
+        if self.camera_config.publish_graph_img:
             graph_img = graph_relations.draw_network_x()
         
         # drawing stuff
@@ -207,7 +259,7 @@ class ObjectDetection:
             # draw the cuboids (makers)
             if detection.tf is not None and detection.obb is not None:
                 
-                # todo: x and y could be the wrong way around! they should be chosen depending on the rot_quat
+                # TODO: x and y could be the wrong way around! they should be chosen depending on the rot_quat
                 changing_height = np.linalg.norm(detection.obb[0]-detection.obb[1])
                 changing_width = np.linalg.norm(detection.obb[1]-detection.obb[2])
                 
@@ -216,6 +268,13 @@ class ObjectDetection:
                 if changing_height < changing_width:
                     height = changing_width
                     width = changing_height
+                
+                #! JSI added this implementation instead: Which one is correct? Why do we even need it?                    
+                # o = detection.obb
+                # changing_height = np.linalg.norm((o[0]-o[2], o[1] - o[3]))
+                # changing_width = np.linalg.norm((o[2] - o[4], o[3] - o[5]))
+                # height = np.min((changing_height, changing_width))
+                # width = np.max((changing_height, changing_width))    
                 
                 marker = self.make_marker(detection.tf, height, width, self.object_depth, detection.id, detection.label)
                 markers.markers.append(marker)

pipeline_camera.py
@@ -32,8 +32,6 @@ from context_action_framework.msg import Detection as ROSDetection
 from context_action_framework.msg import Detections as ROSDetections
 from context_action_framework.types import detections_to_ros, gaps_to_ros, Label, Camera
 
-from obb import obb_px_to_quat
-
 
 class PipelineCamera:
     def __init__(self, yolact, dataset, object_reid, config, camera_config, camera_type, static_tf_manager):
@@ -60,6 +58,9 @@ class PipelineCamera:
         # don't automatically start
         self.continuous_mode = False
         self.single_mode = False
+        
+        # is gaps requested?
+        self.single_mode_request_gaps = False
         
         # time of single_mode call
         self.single_mode_time = None
@@ -118,7 +119,7 @@ class PipelineCamera:
         rospy.on_shutdown(self.exit)
     
     def init_pipeline(self, yolact, dataset, object_reid):
-        self.object_detection = ObjectDetection(self.config, yolact, dataset, object_reid, self.camera_type, self.parent_frame)
+        self.object_detection = ObjectDetection(self.config, self.camera_config, yolact, dataset, object_reid, self.camera_type, self.parent_frame)
         self.worksurface_detection = None
 
     def create_subscribers(self):
@@ -166,14 +167,12 @@ class PipelineCamera:
         self.img_msg = img_msg
         self.img_id += 1
         
-        if self.single_mode:
-            if self.processing_acquisition_stamp is None:
-                t = rospy.get_rostime().to_sec()
-                # time since single mode called
-                img_age = np.round(t - self.acquisition_stamp.to_sec(), 2)
-                single_mode_age = str(np.round(t - self.single_mode_time, 2))
-                img_age_since_single_mode = str(np.round(self.acquisition_stamp.to_sec() - self.single_mode_time, 2))
-                print("[blue]"+ self.camera_name +" (single_mode): received img from camera, img_id:" + str(self.img_id) + ", img_age: "+ str(img_age) +", single_mode_age: "+ single_mode_age+", img_age_since_single_mode: "+img_age_since_single_mode+"[/blue]")
+        t = rospy.get_rostime().to_sec()
+        img_age = np.round(t - self.acquisition_stamp.to_sec(), 2)
+        
+        if img_age < -0.1:
+            print("[red]"+self.camera_name +": img arrived with timestamp in the future by 100ms!")
+
       
     def enable_camera_cb(self, req):
         state = req.data
@@ -198,9 +197,13 @@ class PipelineCamera:
         msg = "debug: " + ("enabled" if state else "disabled")
         return True, msg
     
-    # TODO: return false if the cameras aren't running... otherwise the service call will hang forever
     def vision_single_det_cb(self, req):      
         request_gap_detection = req.gap_detection
+        self.single_mode_request_gaps = request_gap_detection
+        
+        if not self.camera_enabled:
+            print("[red]"+self.camera_name +" (single mode): Enable cameras before requesting detection.")
+            return VisionDetectionResponse(False, VisionDetails(), None)
               
         print(self.camera_name +": getting detection")
         self.single_mode = True
@@ -213,10 +216,8 @@ class PipelineCamera:
         t = rospy.get_rostime().to_sec()
  
         img_age = np.round(t - self.processed_acquisition_stamp.to_sec(), 2)
-        single_mode_age = str(np.round(t - single_mode_time, 2))
-        print("[blue]"+self.camera_name +" (single_mode): returning single detection, img_id:" + str(self.processed_img_id) + ", img_age: "+ str(img_age) +", single_mode_age: "+ single_mode_age+"[/blue]")
+        print("[blue]"+self.camera_name +" (single_mode): returning single detection, img_id:" + str(self.processed_img_id) + ", img_age: "+ str(img_age) +"[/blue]")
         
-        # TODO: also only process gap stuff if it is requested... otherwise it is time-wasting
         ros_gaps = []
         will_return_gaps = False
         if self.gaps is not None and request_gap_detection:
@@ -227,10 +228,10 @@ class PipelineCamera:
             header = rospy.Header()
             header.stamp = rospy.Time.now()
             vision_details = VisionDetails(header, self.processed_acquisition_stamp, self.camera_type, will_return_gaps, detections_to_ros(self.detections), ros_gaps)
-            return VisionDetectionResponse(True, vision_details, CvBridge().cv2_to_imgmsg(self.labelled_img))
+            return VisionDetectionResponse(True, vision_details, CvBridge().cv2_to_imgmsg(self.labelled_img, encoding="bgr8"))
         else:
             print(self.camera_name +": returning empty response!")
-            return VisionDetectionResponse(False, VisionDetails(), CvBridge().cv2_to_imgmsg(self.labelled_img))
+            return VisionDetectionResponse(False, VisionDetails(), CvBridge().cv2_to_imgmsg(self.labelled_img, encoding="bgr8"))
     
     def enable_continuous_cb(self, req):
         state = req.data
@@ -273,8 +274,11 @@ class PipelineCamera:
                     msg += "FAILED to disable camera"
             
         except rospy.ServiceException as e:
-            if "UVC device is streaming" in str(e):
+            if "UVC device is streaming" in str(e) and state:
                 msg += "enabled camera (already streaming)"
+                success = True
+            elif "UVC device is not streaming" in str(e) and not state:
+                msg += "disabled camera (already stopped)"
                 success = True
             else:
                 if state:
@@ -305,20 +309,21 @@ class PipelineCamera:
         if self.single_mode:
             t = rospy.get_rostime().to_sec()
             img_age = np.round(t - self.acquisition_stamp.to_sec(), 2)
-            single_mode_age = str(np.round(t - self.single_mode_time, 2))
-            img_age_since_single_mode = str(np.round(self.acquisition_stamp.to_sec() - self.single_mode_time, 2))
-            # print("[blue]"+self.camera_name + " (single_mode): could process, img_id: " + str(self.img_id) + ", img_age: "+ str(img_age) +", single_mode_age: "+ single_mode_age+", img_age_since_single_mode: "+img_age_since_single_mode+"[/blue]")
             
-            if self.single_mode_time < self.acquisition_stamp.to_sec():
-                print("[blue]"+self.camera_name +" (single_mode): about to process, img_id:" + str(self.img_id) + ", img_age: "+ str(img_age) +", single_mode_age: "+ single_mode_age+", img_age_since_single_mode: "+img_age_since_single_mode+"[/blue]")
+            time_leaway = 0.05 # if the frame arrived very slightly before single_mode_time, then still process it
+            if self.single_mode_time < self.acquisition_stamp.to_sec() + time_leaway:
+                print("[blue]"+self.camera_name +" (single_mode): about to process, img_id:" + str(self.img_id) + ", img_age: "+ str(img_age) + "[/blue]")
                 single_mode_frame_accepted = True
-            # else:
-            #     print("[red]"+self.camera_name +" (single_mode): invalid, img_id:" + str(self.img_id) + ", img_age: "+ str(img_age) +", single_mode_age: "+ single_mode_age+", img_age_since_single_mode: "+img_age_since_single_mode+"[/red]")
+
         
         # process frame if in continuous mode or if single mode frame is accepted
         if self.continuous_mode or single_mode_frame_accepted or self.is_first_frame:
             
-            success = self.run_frame()
+            compute_gaps = True # TODO: we could make this a parameter
+            if single_mode_frame_accepted:
+                compute_gaps = self.single_mode_request_gaps
+            
+            success = self.run_frame(compute_gaps)
 
             # compute the first frame immediately
             if success:
@@ -341,7 +346,7 @@ class PipelineCamera:
             self.rate_limit_single.sleep()
             
     # this has to be run on the main thread
-    def run_frame(self):
+    def run_frame(self, compute_gaps=False):
 
         if self.img_msg is None:
             #print(self.camera_name +": Waiting to receive image.")
@@ -369,7 +374,6 @@ class PipelineCamera:
 
         # All the checks passes, run the pipeline
         #! we should now lock these variables from the callback
-
         
         processing_img_id = self.img_id
         # processing_colour_img = np.copy(self.colour_img) #? unused
@@ -379,8 +383,7 @@ class PipelineCamera:
         if t_prev is not None and self.last_run_time - t_prev > 0:
             fps = "fps_total: " + str(round(1 / (self.last_run_time - t_prev), 1)) + ", "
 
-        # TODO: don't know if remaining_args works
-        labelled_img, detections, markers, poses, graph_img, *remaining_args = self.process_img(fps)
+        labelled_img, detections, markers, poses, graph_img, *remaining_args = self.process_img(fps, compute_gaps)
 
         self.publish(labelled_img, detections, markers, poses, graph_img, *remaining_args)
         
@@ -400,13 +403,14 @@ class PipelineCamera:
         if self.img_id == sys.maxsize:
             self.img_id = 0
             self.processed_img_id = -1
+            
         
-        print("[green]"+self.camera_name +": published img: "+ str(processing_img_id) +", num. dets: " + str(len(detections)) + "[/green]")
+        print("[green]"+self.camera_name +": published img: "+ str(processing_img_id) +", num. dets: " + str(len(detections)) + ", delay: " + str(np.round(self.processed_delay, 2)) + "[/green]")
         
         return True
     
 
-    def process_img(self, fps=None, camera_info=None, depth_img=None):
+    def process_img(self, fps=None, camera_info=None, depth_img=None, compute_gaps=False):
         colour_img = np.array(CvBridge().imgmsg_to_cv2(self.img_msg, "bgr8"))
         self.colour_img = rotate_img(colour_img, self.camera_config.rotate_img)
         
@@ -465,7 +469,6 @@ class PipelineCamera:
         self.poses_pub.publish(poses)
     
         # Publish the TFs
-        #! TFs seem incorrect for Realsense camera
         self.publish_transforms(detections, timestamp)
         
         return header, timestamp
@@ -480,16 +483,6 @@ class PipelineCamera:
                 # t.child_frame_id = "obj_"+ str(detection.id)
                 t.child_frame_id = '%s_%s_%s'%(Label(detection.label).name, detection.id, self.camera_config.parent_frame)
                 t.transform = detection.tf
-                
-                
-                # translation = copy.deepcopy(detection.tf.translation)   # Table should be rotated in config but that is a lot more work
-                # translation.z = 0 #? why not at 2.5cm or whatever for basler? What about for realsense?
-                # tr = (translation.x, translation.y, translation.z)
-                
-                # rotation = detection.tf.rotation
-                # rotation = [rotation.x, rotation.y, rotation.z, rotation.w]
-                
-                
-                # self.tf_broadcaster.sendTransform(tr, rotation, timestamp, child_frame, self.camera_config.parent_frame)
+
                 self.tf_broadcaster.sendTransform(t)
                 