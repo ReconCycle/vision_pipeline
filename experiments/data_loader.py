2befbd8f1e8e85bf85d789781ae2d3b8d9f4f44d
Sebastian Ruiz <sruiz@mailbox.org>
2023-02-17 10:58:25 +0100

    experiments

---
+++ experiments/data_loader.py
@@ -0,0 +1,263 @@
+from torchvision import datasets
+import torch
+# from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+import torchvision.transforms as transforms
+# import albumentations as A
+# import albumentations.pytorch
+import PIL.Image as Image
+import numpy as np
+import cv2
+import os
+import regex as re
+from tqdm import tqdm
+from rich import print
+
+
+class ImageDataset(datasets.ImageFolder):
+    def __init__(self, main_dir, class_dirs, unseen_class_offset=0, transform=None, exemplar_transform=None, limit_imgs_per_class=None):
+        self.main_dir = main_dir
+        self.class_dirs = class_dirs
+        self.unseen_class_offset = unseen_class_offset
+        self.exemplar_transform = exemplar_transform
+        
+        if limit_imgs_per_class is not None:
+            print("\n"+"="*20)
+            print("Imgs per class limited to", limit_imgs_per_class)
+            print("="*20, "\n")
+            # to make sure each class is of approximately the same size, we set the number of images per class to 30       
+            def is_valid_file(file_path):
+                file_name = os.path.basename(file_path)
+                num = int(re.findall(r'\d+', file_name)[-1])
+                
+                if num < limit_imgs_per_class:
+                    return True
+                else:
+                    return False
+        else:
+            is_valid_file = None
+        
+        
+        super(ImageDataset, self).__init__(main_dir, transform, is_valid_file=is_valid_file)
+        
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, label) where target is class_index of the target class.
+        """
+        path, label = self.samples[index]
+
+        sample = Image.open(path) # use PIL to work with pytorch transforms
+        sample = np.array(sample)
+
+        # only rgb if it is an rgbd image
+        sample = sample[:, :, :3]
+
+        if self.transform is not None:
+            # convert back to PIL
+            sample = Image.fromarray(sample)
+            sample = self.transform(sample)
+
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+            
+        label = label + self.unseen_class_offset
+
+        # also return an examplar image of that class (maybe useful for the autoencoder)
+        item_class_name = os.path.dirname(path)
+        exemplar_path = os.path.join(self.main_dir, item_class_name + ".png")
+        exemplar = None
+        if os.path.isfile(exemplar_path):
+            exemplar = Image.open(exemplar_path)
+            if self.exemplar_transform is not None:
+                exemplar = self.transform(exemplar)
+
+        #! do here one time per image processing
+        
+        # return sample, label, exemplar, path
+        return sample, label, path
+        
+    # restrict classes to those in subfolder_dirs
+    def find_classes(self, dir: str):
+        classes = [d.name for d in os.scandir(dir) if d.is_dir() and d.name in self.class_dirs]
+        classes.sort()
+        # print("Directories in this dataset:", classes)
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+    
+    
+class DataLoader():
+    def __init__(self, 
+                 img_dir, 
+                 batch_size=16, 
+                 validation_split=.2,
+                 shuffle=True,
+                 shuffle_train_val_split=True,
+                 seen_classes=["hca_0", "hca_1", "hca_2", "hca_3", "hca_4", "hca_5", "hca_6"],
+                 unseen_classes=["hca_7", "hca_8", "hca_9", "hca_10", "hca_11", "hca_12"],
+                 transform=None,
+                 limit_imgs_per_class=None):
+        
+        random_seed= 42
+        
+        self.img_dir = img_dir    
+        self.batch_size = batch_size
+        
+        # use seen_classes and unseen_classes to specify which directories to load
+        #? have more training data by giving it more classes (for now)
+        self.seen_classes = seen_classes
+        self.unseen_classes = unseen_classes
+
+        # seen_dirs = [folder_prefixes + str(class_num) for class_num in seen_classes]
+        # unseen_dirs = [folder_prefixes + str(class_num) for class_num in unseen_classes]
+        self.seen_dirs = seen_classes
+        self.unseen_dirs = unseen_classes
+        
+        #! albumentations is doing funny things with the normalisation. Let's use pytorch inbuilt thingy first.
+        # transform is passed to dataloader
+        # transform = transforms.Compose([
+        #     transforms.Resize((64, 64)),
+        #     transforms.ToTensor(),
+        #     # computed transform using compute_mean_std() to give:
+        #     transforms.Normalize(mean=[0.5895, 0.5935, 0.6036],
+        #                         std=[0.1180, 0.1220, 0.1092])
+        # ])
+
+        seen_dataset = ImageDataset(img_dir, 
+                               self.seen_dirs, 
+                               transform=transform, 
+                               exemplar_transform=transform, 
+                               limit_imgs_per_class=limit_imgs_per_class)
+
+        # Create data indices for training and validation splits
+        dataset_size = len(seen_dataset)
+        indices = list(range(dataset_size))
+        split = int(np.floor(validation_split * dataset_size))
+        if shuffle_train_val_split:
+            np.random.seed(random_seed)
+            np.random.shuffle(indices)
+        seen_train_indices, seen_val_indices = indices[split:], indices[:split]
+        indices = {"seen_train": seen_train_indices,
+                "seen_val": seen_val_indices}
+        
+        #! I don't think the train/val split is random!!
+        
+        # Create train and validation datasets
+        self.datasets = {x: torch.utils.data.Subset(seen_dataset, indices[x])
+                    for x in ["seen_train", "seen_val"]}
+        
+        # add unseen dataset
+        self.datasets["unseen_val"] = ImageDataset(img_dir, 
+                                      self.unseen_dirs, 
+                                      unseen_class_offset=len(seen_dataset.classes), 
+                                      transform=transform, 
+                                      limit_imgs_per_class=limit_imgs_per_class)
+        
+        # concat seen_val and unseen_val datasets
+        self.datasets["val"] = torch.utils.data.ConcatDataset([self.datasets["seen_val"], self.datasets["unseen_val"]])
+        
+        # create the dataloaders
+        # todo: fix bug, either requiring: generator=torch.Generator(device='cuda'),
+        # todo: or requiring shuffle=False
+        self.dataloaders = {x: torch.utils.data.DataLoader(self.datasets[x], 
+                                                           num_workers=0, 
+                                                           batch_size=batch_size,
+                                                           generator=torch.Generator(device='cuda'),
+                                                           shuffle=shuffle)
+                            for x in ["seen_train", "seen_val", "unseen_val", "val"]}
+        
+        self.dataset_sizes = {x: len(self.datasets[x]) for x in ["seen_train", "seen_val", "unseen_val", "val"]}
+        
+        # class names for train/val, unseen, unseen+val/all
+        self.classes = {
+            "seen_train": seen_dataset.classes,
+            "seen_val": seen_dataset.classes,
+            "unseen_val": self.datasets["unseen_val"].classes,
+            "val": np.concatenate((seen_dataset.classes, self.datasets["unseen_val"].classes)),
+            "all": np.concatenate((seen_dataset.classes, self.datasets["unseen_val"].classes))
+        }
+        
+        # self.img_shape = list(self.datasets["train"][0][0].shape)
+        
+        
+    def compute_mean_std(self):
+        # compute mean and std for training dataset of images. From here the normalisation values can be set.
+        # for example:
+        # A.Normalize(mean=(154.5703, 148.4985, 152.1174), std=(31.3750, 29.3120, 29.2421), max_pixel_value=1),
+        # after setting this, the (mean, std) should be (0, 1)
+        
+        batches = []
+        for sample, label, *_ in tqdm(self.dataloaders["seen_train"]):
+            batches.append(sample)
+        
+        all_imgs = torch.cat(batches)
+        all_imgs = torch.Tensor.float(all_imgs)
+        
+        print("all_imgs.shape", all_imgs.shape)
+        index_channels = all_imgs.shape.index(3)
+        if index_channels != 3:
+            # channels are not in the last dimension
+            # move the channels to the last dimension:
+            all_imgs = torch.transpose(all_imgs, index_channels, -1).contiguous() 
+        
+        if all_imgs.shape[-1] != 3:
+            print("\nERROR: the last dimension should be the channels!\n")
+        
+        print("all_imgs.shape", all_imgs.shape)
+        
+        all_imgs = all_imgs.view(-1, 3)
+        
+        print("all_imgs.shape", all_imgs.shape)
+        
+        min = all_imgs.min(dim=0).values
+        max = all_imgs.max(dim=0).values
+        
+        print("min", min)
+        print("max", max)
+        
+        mean = all_imgs.mean(dim=0)
+        std = all_imgs.std(dim=0)
+        print("mean", mean)
+        print("std", std)
+    
+    def example_iterate(self, type_name="seen_train"):
+        label_distribution = {}
+        for a_class in self.classes[type_name]:
+            label_distribution[a_class] = 0
+        
+        for i, (inputs, labels) in enumerate(self.dataloaders[type_name]):
+            # print("i", i)
+            # print("inputs.shape", inputs.shape)
+            # print("labels", labels)
+            # print("")
+
+            labels_name = [self.classes[type_name][label] for label in labels]
+
+            for j in np.arange(len(labels)):
+                label_distribution[labels_name[j]] += 1
+
+        return label_distribution
+
+    def example(self):
+        
+        print("classes:", self.classes)
+        print("")
+
+        label_dist_seen_train = dataloader.example_iterate("seen_train")
+        label_dist_seen_val = dataloader.example_iterate("seen_val")
+
+        print("label_dist_seen_train", label_dist_seen_train)
+        print("label_dist_seen_val", label_dist_seen_val)
+
+
+        
+        
+if __name__ == '__main__':
+    print("Run this for testing the dataloader only.")
+    img_dir = "/home/sruiz/datasets2/reconcycle/simon_rgbd_dataset/hca_simon/sorted_in_folders"
+    dataloader = DataLoader(img_dir)
+    dataloader.compute_mean_std()
+    dataloader.example()
---
+++ experiments/data_loader_even_pairwise.py
@@ -0,0 +1,300 @@
+from torchvision import datasets
+import torch
+# from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+# import albumentations as A
+# import albumentations.pytorch
+import numpy as np
+import cv2
+import os
+import regex as re
+from tqdm import tqdm
+import math
+from collections import Counter
+import sys
+import itertools
+import functools
+import operator
+from rich import print
+import utils as utils
+
+from data_loader import DataLoader
+# from data_loader_mnist import DataLoaderMNIST
+
+# inspiration:
+# https://discuss.pytorch.org/t/how-to-take-pairwise-input-image-after-combing-the-models/124402/5
+# https://colab.research.google.com/github/rramosp/2021.deeplearning/blob/master/content/U2%20LAB%2003%20-%20Pairwise%20image%20classification.ipynb#scrollTo=fU3h0AWM3ChA
+
+
+def nC2(n):
+    return int((n*(n-1))/2)
+
+def pair(n, k):
+    """Returns the pair given the number of items, from the total number of pairs nC2
+    
+    See: https://math.stackexchange.com/questions/4046051/how-to-find-the-nth-pair-from-a-generated-list-of-pairs
+    
+    The total number of pairs is nC2.
+    Pairs are ordered as:
+    (0,1),(0,2),...,(0,n-1),(1,2),(1,3),...,(1,n-1),...,(n-2, n-1)
+
+    Args:
+        n (int): Number of items
+        k (int): Pair id. In range 0, ..., (nC2)-1
+
+    Returns:
+        int, int: the pair
+    """
+    # if k > nC2(n) - 1:
+        # raise SystemExit("k out of range", k, nC2(n) - 1)
+    
+    # inside = 4*n*n-4*n+1-8*k
+    # if inside < 0:
+    #     raise SystemExit("negative value in sqrt", inside, "k", k, "n", n)
+    x = math.floor((2*n-1-math.sqrt(4*n*n-4*n+1-8*k))/2)
+    y = k - x*n + x*(x+1)//2 + x + 1
+    return x, y
+
+def pair_idx(n, x, y):
+    """Inverse of pair(n, k). Given a pair it returns the index
+
+    Args:
+        n (int): number of items
+        x (int): item x. In range 0, ..., n-1
+        y (int): item y. In range 0, ..., n-1
+
+    Returns:
+        int: pair index. In range 0, ..., (nC2)-1
+    """
+    if x == y:
+        return None
+    if x > y:
+        x, y = y, x
+    
+    k = y-(x+1) + x*n-((x*(x+1))/2)
+    return k
+
+
+
+
+
+def nth_item(n, item, iterable):
+    """find the index of the n'th occurrence of an item in a list
+    source: https://stackoverflow.com/questions/8337069/find-the-index-of-the-nth-item-in-a-list
+
+    Args:
+        n (int): nth occurance
+        item (int): of this item
+        iterable (list): in this list
+
+    Returns:
+        int: the index of the n'th occurance of the item in the list
+    """
+    indicies = itertools.compress(itertools.count(), map(functools.partial(operator.eq, item), iterable))
+    return next(itertools.islice(indicies, n, None), -1)
+    
+
+
+class EvenPairwiseDataset(torch.utils.data.Dataset):
+    """Gives an even split of sample pairs with the same label, as sample pairs with different labels
+
+    Args:
+        torch (dataset): dataset of items providing (sample, label, _*)
+    """
+    def __init__(self, dataset):
+        # to get the dataset from a dataloader, use: dataloader.dataset
+    
+        self.dataset = dataset
+        # n is number of items
+        self.n = len(dataset)
+        # print("self.n", self.n)
+        self.dataset_labels = [label for _, label in dataset]
+        self.samples_per_class = Counter(self.dataset_labels)
+        # print("samples_per_class", self.samples_per_class)
+        
+        
+        # compute total number of positive pairs:
+        self.pairs_per_class = {}
+        self.num_positive_pairs = 0
+        for class_id, num_samples in self.samples_per_class.items():
+            # print("samples", num_samples)
+            num_pairs = nC2(num_samples)
+            # print("num_pairs")
+            self.pairs_per_class[class_id] = num_pairs
+            self.num_positive_pairs += num_pairs
+            
+    def __len__(self):
+        # we want 50/50 split of positive/negative pairs
+        # the dataset length can't exceed nC2
+        return np.minimum(2*self.num_positive_pairs, nC2(self.n))
+
+    def __getitem__(self, idx):
+        # we first index over all the negative pairs, then over all the positive pairs
+        # print("")
+        # print("self.n", self.n)
+        # print("self.samples_per_class", self.samples_per_class)
+        # print("self.pairs_per_class", self.pairs_per_class)
+        # print("self.num_positive_pairs", self.num_positive_pairs)
+        # print("")
+        # print("idx", idx)
+        id1 = -1
+        id2 = -1
+        
+        sum = 0
+        positive_pair = False
+        for class_id, num_pairs in self.pairs_per_class.items():
+        # for i in np.arange(len(self.pairs_per_class)):
+            if idx < sum + num_pairs:
+                # print("positive pair")
+                positive_pair = True
+                # we are in this pair
+                rel_idx = idx - sum
+                # print("class_id", class_id)
+                # print("num_pairs", num_pairs)
+                # print("rel_idx", rel_idx)
+                
+                x1, x2 = pair(self.samples_per_class[class_id], rel_idx) # from n items, get the kth pair
+                
+                # print("x1", x1)
+                # print("x2", x2)
+                
+                # the dataset isn't ordered, so we need to find the 'x1'th sample with label 'class_id' in the dataset
+                id1 = nth_item(x1, class_id, self.dataset_labels)
+                id2 = nth_item(x2, class_id, self.dataset_labels)
+                break
+            else:
+                sum += num_pairs
+        
+        if not positive_pair:
+            # this SHOULD be the same as: if idx > self.num_positive_pairs:
+            # print("self.num_positive_pairs", self.num_positive_pairs)
+            # print("should generate negative pair, or generate with high probability")
+            # most pairs are negative, so let's return a pair
+            # ? there is probably a way to guarantee we get a negative pair, but close enough for now
+            id1, id2 = pair(self.n, idx)
+            
+        # print("id1", id1)
+        # print("id2", id2)
+        
+        if id1 == -1 or id2 == -1:
+            raise SystemExit("id1 or id2 is negative")
+        
+        sample1, label1, *_ = self.dataset[id1]
+        sample2, label2, *_ = self.dataset[id2]
+        
+        
+        # print("label1", label1)
+        # print("label2", label2)
+        # print("")
+        
+        if positive_pair and label1 != label2:
+            raise SystemExit("the labels of the pair should be the same, but they aren't")
+        
+        return sample1, label1, sample2, label2
+    
+
+    
+class DataLoaderEvenPairwise():
+    """dataloader for EvenPairwiseDataset
+    """
+    def __init__(self, 
+                 img_dir="MNIST", 
+                 batch_size=256, 
+                 shuffle=True, 
+                 validation_split=.2,
+                 seen_classes = ["hca_0", "hca_1", "hca_2", "hca_3", "hca_4", "hca_5", "hca_6"],
+                 unseen_classes = ["hca_7", "hca_8", "hca_9"],
+                 transform=None):
+        
+        if img_dir == "MNIST":
+            raise NotImplementedError
+            # dataloader_imgs = DataLoaderMNIST(batch_size=batch_size, 
+            #                                   shuffle=shuffle,
+            #                                   validation_split=validation_split,
+            #                                   seen_classes=seen_classes,
+            #                                   unseen_classes=unseen_classes)
+
+        else:
+            dataloader_imgs = DataLoader(img_dir, 
+                                         batch_size=batch_size, 
+                                         shuffle=shuffle,
+                                         validation_split=validation_split,
+                                         seen_classes=seen_classes,
+                                         unseen_classes=unseen_classes,
+                                         limit_imgs_per_class=30,
+                                         transform=transform) #! why limit?
+
+        self.classes = dataloader_imgs.classes
+        self.dataloaders = {x: torch.utils.data.DataLoader(
+                                    EvenPairwiseDataset(dataloader_imgs.datasets[x]), 
+                                    batch_size=batch_size, 
+                                    shuffle=shuffle
+                                )
+                            for x in ["seen_train", "seen_val", "unseen_val", "val"]}
+        
+    def example(self):
+        positive_pairs = 0
+        negative_pairs = 0
+        label_distribution = {}
+        
+        # classes_seen_train = self.classes["seen_train"]
+        for a_class in self.classes["seen_train"]:
+            label_distribution[a_class] = 0
+
+        print("classes seen_train", self.classes["seen_train"])
+
+        for i, (sample1, label1, sample2, label2) in enumerate(self.dataloaders["seen_train"]):
+            # print("\ni", i)
+            # print("i % 100", i % 100)
+            # print("sample1.shape", sample1.shape)
+            # print("sample2.shape", sample2.shape)
+            # print("label1.shape", label1.shape)
+            # print("label2.shape", label2.shape)
+            label1 = label1.detach().numpy()
+            label2 = label2.detach().numpy()
+            label1 = [self.classes["seen_train"][label] for label in label1]
+            label2 = [self.classes["seen_train"][label] for label in label2]
+
+
+            for j in np.arange(len(label1)):
+                if label1[j] == label2[j]:
+                    positive_pairs += 1
+                else:
+                    negative_pairs += 1
+                    
+                if label1[j] in label_distribution.keys():
+                    label_distribution[label1[j]] += 1
+                else:
+                    label_distribution[label1[j]] = 0
+                    
+            for j in np.arange(len(label2)):
+                if label2[j] in label_distribution.keys():
+                    label_distribution[label2[j]] += 1
+                else:
+                    label_distribution[label2[j]] = 0
+                
+            
+            if i == 0:
+                print("\nbatch num:", i)
+                print("positive_pairs", positive_pairs)
+                print("negative_pairs", negative_pairs)
+                print("label_distribution", label_distribution)
+            # break # debug
+        
+        print("")
+        print("distributions:")
+        print("\nbatch num:", i)
+        print("positive_pairs", positive_pairs)
+        print("negative_pairs", negative_pairs)
+        print("label_distribution", label_distribution)
+        
+        
+        
+if __name__ == '__main__':
+    print("Run this for testing the dataloader only.")
+    img_dir = "/home/sruiz/datasets2/reconcycle/simon_rgbd_dataset/hca_simon/sorted_in_folders"
+    # img_dir = "/home/sruiz/datasets/labelme/hca_front_21-10-01/cropped"
+    # img_dir = "MNIST"
+    utils.init_seeds(1, cuda_deterministic=False)
+    dataloader = DataLoaderEvenPairwise(img_dir, 32, shuffle=True)
+    dataloader.example()
